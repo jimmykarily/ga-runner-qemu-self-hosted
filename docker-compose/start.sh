7e846bd5e9a0a8027143b4eb3cecb0f09cc84936
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-06-30 17:47:03 +0300

    Use action-runner-controller images
    This doesn't work better but commiting it for reference. Things that
    don't work:
    
    - It also fails to start dockerd because if finds the
      /var/run/docker.pid in place (not sure why, but that's the issue I was
      trying to solve)
    - It doesn't cleanup workers when the containers get stopped. This is
      supposed to be a responsibility of the controller in Kubernetes but we
      are not in Kubernetes.
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- docker-compose/start.sh
+++ docker-compose/start.sh
@@ -1,60 +1,17 @@
 #!/bin/bash
 
-GH_OWNER=$GH_OWNER
-GH_REPOSITORY=$GH_REPOSITORY
-GH_TOKEN=$GH_TOKEN
-
-RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
-RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
-
-export RUNNER_ALLOW_RUNASROOT=true
-
-function wait_for_process () {
-    local max_time_wait=30
-    local process_name="$1"
-    local waited_sec=0
-    while ! pgrep "$process_name" >/dev/null && ((waited_sec < max_time_wait)); do
-        echo "Process $process_name is not running yet. Retrying in 1 seconds"
-        echo "Waited $waited_sec seconds of $max_time_wait seconds"
-        sleep 1
-        ((waited_sec=waited_sec+1))
-        if ((waited_sec >= max_time_wait)); then
-            return 1
-        fi
-    done
-    return 0
-}
-
 REG_TOKEN=$(curl -sX POST -H "Accept: application/vnd.github.v3+json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/${GH_OWNER}/${GH_REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
-echo "Starting docker daemon"
-sudo sh -c 'sudo /usr/bin/dockerd > dockerd-output 2>&1' &
-echo "Waiting for dockerd to be running"
-if ! wait_for_process "dockerd"; then
-    echo "dockerd is not running after max time"
-    echo "This was the output:"
-    cat dockerd-output
-    exit 1
-else
-    echo "dockerd is running"
-fi
+RUNNER_SUFFIX=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 5 | head -n 1)
 
-cd /home/runner/actions-runner
+export RUNNER_ALLOW_RUNASROOT=true
+export RUNNER_ORG=$GH_OWNER
+export RUNNER_REPO=$GH_REPOSITORY
+export RUNNER_TOKEN=$REG_TOKEN
+# Hack to get to this block:
+# https://github.com/actions/actions-runner-controller/blob/d134dee14b5ecd76ecbd2b3b2e1bc79fe3f4a95d/runner/startup.sh#L36
+export RUNNER_ENTERPRISE="dummy"
+export RUNNER_NAME="dockerNode-${RUNNER_SUFFIX}"
+export RUNNER_EPHEMERAL=true
 
-echo "Starting runner with name ${RUNNER_NAME}"
-./config.sh --unattended --ephemeral --url https://github.com/${GH_OWNER}/${GH_REPOSITORY} --token ${REG_TOKEN} --name ${RUNNER_NAME}
-
-cleanup() {
-    echo "Removing runner ${RUNNER_NAME}"
-    ./config.sh remove --unattended --token ${REG_TOKEN}
-}
-
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
-
-# Allow the "runner" user to access /dev/kvm
-# Might not be the best solution but adding to the kvm group didn't work
-# https://askubuntu.com/a/1081326
-sudo setfacl -m u:runner:rwx /dev/kvm
-
-./run.sh & wait $!
+entrypoint-dind.sh
